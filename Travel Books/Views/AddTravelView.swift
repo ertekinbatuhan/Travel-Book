3da9142678840ee6c7401ab0900e21c71bccb9d9
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-07-17 11:30:41 +0300

    update

--- Travel Books/Views/AddTravelView.swift
+++ Travel Books/Views/AddTravelView.swift
@@ -4,95 +4,95 @@ import PhotosUI
 struct AddTravelView: View {
     @StateObject private var viewModel = TravelViewModel()
     @State private var photosPickerItem: PhotosPickerItem?
-
+  
     var body: some View {
         NavigationStack {
-            VStack(spacing: 20) {
-                PhotosPicker(selection: $photosPickerItem, matching: .any(of: [.images, .videos, .screenshots])) {
-                    if let image = viewModel.image {
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(maxWidth: .infinity, maxHeight: 200)
-                            .cornerRadius(10.0)
-                            .shadow(radius: 10)
-                            .padding(.horizontal, 20)
-                            .clipped()
-                    } else {
-                        Image(systemName: "photo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: .infinity, maxHeight: 300)
-                            .foregroundColor(.gray)
-                            .cornerRadius(10.0)
-                            .shadow(radius: 10)
-                            .padding(.horizontal, 20)
-                    }
-                }
-
-                TextField("Enter Travel Name", text: $viewModel.travelName)
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8.0)
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(.blue)
-
-                TextEditor(text: $viewModel.travelDescription)
-                    .scrollContentBackground(.hidden)
-                    .padding()
-                    .background(Color.gray.opacity(0.15))
-                    .cornerRadius(12)
-                    .foregroundColor(.blue)
-                    .tint(Color.yellow)
-
+            ScrollView {
                 VStack(spacing: 20) {
-                    HStack {
-                        Text("Your Rating")
-                            .font(.title3)
-                            .foregroundColor(.gray)
-                        Spacer()
-                        StarRatingView(rating: $viewModel.rating)
+                    PhotosPicker(selection: $photosPickerItem, matching: .any(of: [.images, .videos, .screenshots])) {
+                        if let image = viewModel.image {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: .infinity, maxHeight: 400)
+                                .cornerRadius(12.0)
+                                .shadow(radius: 10)
+                                
+                        } else {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: .infinity, maxHeight: 300)
+                                .foregroundColor(.gray)
+                                .cornerRadius(12.0)
+                                .shadow(radius: 10)
+                                .padding(.horizontal)
+                        }
                     }
-                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .center)
 
-                    HStack {
-                        Text("Select Date")
-                            .font(.title3)
-                            .foregroundColor(.gray)
-                        Spacer()
-                        DatePicker("", selection: $viewModel.selectedDate, displayedComponents: .date)
-                            .datePickerStyle(CompactDatePickerStyle())
+                    TextField("Enter Travel Name", text: $viewModel.travelName)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8.0)
+                        .foregroundColor(.blue)
+
+                    TextField("Enter Travel Description", text: $viewModel.travelDescription)
+                        .padding()
+                        .background(Color.gray.opacity(0.15))
+                        .cornerRadius(12)
+                        .foregroundColor(.blue)
+                        .lineLimit(nil)
+                        .tint(Color.yellow)
+
+                    VStack(spacing: 20) {
+                        HStack {
+                            Text("Your Rating")
+                                .font(.title3)
+                                .foregroundColor(.gray)
+                            Spacer()
+                            StarRatingView(rating: $viewModel.rating)
+                        }
+                        .padding(.horizontal)
+
+                        HStack {
+                            Text("Select Date")
+                                .font(.title3)
+                                .foregroundColor(.gray)
+                            Spacer()
+                            DatePicker("", selection: $viewModel.selectedDate, displayedComponents: .date)
+                                .datePickerStyle(CompactDatePickerStyle())
+                        }
+                        .padding(.horizontal)
                     }
+
+                    Button("Save") {
+                        viewModel.addTravelItem()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(8.0)
                     .padding(.horizontal)
                 }
-
-                Button("Save") {
-                    viewModel.addTravelItem()
-                }
-                .frame(maxWidth: .infinity)
-                .frame(height: 50)
-                .background(Color.red)
-                .foregroundColor(.white)
-                .cornerRadius(8.0)
-                .padding(.horizontal)
-                .padding(.bottom, 30)
-            }
-            .padding(.horizontal)
-            .onChange(of: photosPickerItem) { _ in
-                Task {
-                    if let photosPickerItem = photosPickerItem,
-                       let data = try? await photosPickerItem.loadTransferable(type: Data.self),
-                       let newImage = UIImage(data: data) {
-                        viewModel.image = newImage
+                .padding()
+                .onChange(of: photosPickerItem) { _ in
+                    Task {
+                        if let photosPickerItem = photosPickerItem,
+                           let data = try? await photosPickerItem.loadTransferable(type: Data.self),
+                           let newImage = UIImage(data: data) {
+                            viewModel.image = newImage
+                        }
+                        photosPickerItem = nil
                     }
-                    photosPickerItem = nil
                 }
+                .alert(isPresented: $viewModel.isSaved) {
+                    Alert(title: Text("Saved"), message: Text("Travel item saved successfully."), dismissButton: .default(Text("OK"), action: viewModel.resetFields))
+                }
+                .navigationTitle("Add New Travel")
+                .navigationBarTitleDisplayMode(.large)
             }
-            .alert(isPresented: $viewModel.isSaved) {
-                Alert(title: Text("Saved"), message: Text("Travel item saved successfully."), dismissButton: .default(Text("OK"), action: viewModel.resetFields))
-            }
-            .navigationTitle("Add New Travel")
-            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
